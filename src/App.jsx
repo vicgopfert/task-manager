535995edec744d3454e7f1c8ed93080d5e8d83e2
Victor Gopfert <gopfert.web@gmail.com>
2026-07-03 13:59:42 -0300

    chore: add tailwind prettier plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import Tasks from './Tasks'
-
 function App() {
   return (
     <div className="App">
-      <Tasks />
+      <h1 className="mt-2 bg-red-500 p-6">Hello world!</h1>
     </div>
   )
 }
