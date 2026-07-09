a8476cc5880528da227ec23ace8f33d9dfe126d0
Victor Gopfert <gopfert.web@gmail.com>
2026-07-09 10:20:42 -0300

    feat: add TimeSelect component

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -6,7 +6,7 @@ import { CSSTransition } from "react-transition-group"
 
 import Button from "./Button"
 import Input from "./Input"
-import InputLabel from "./InputLabel"
+import TimeSelect from "./TimeSelect"
 
 const AddTaskDialog = ({ isOpen, handleClose }) => {
   const nodeRef = useRef(null)
@@ -33,18 +33,7 @@ const AddTaskDialog = ({ isOpen, handleClose }) => {
           <div className="flex w-84 flex-col space-y-4">
             <Input id="title" label="Título" placeholder="Título da tarefa" />
 
-            <div className="flex flex-col gap-1 text-left">
-              <InputLabel htmlFor="time">Horário</InputLabel>
-
-              <select
-                id="time"
-                className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 outline-[#00ADB5] placeholder:text-sm placeholder:text-[#9A9C9F]"
-              >
-                <option value="morning">Manhã</option>
-                <option value="afternoon">Tarde</option>
-                <option value="evening">Noite</option>
-              </select>
-            </div>
+            <TimeSelect />
 
             <Input
               id="description"
@@ -61,6 +50,7 @@ const AddTaskDialog = ({ isOpen, handleClose }) => {
               >
                 Cancelar
               </Button>
+
               <Button size="large" className="w-full">
                 Salvar
               </Button>
