9f74a51e72547bdca79de05e9e70130ef33da28a
Victor Gopfert <gopfert.web@gmail.com>
2026-07-23 10:27:41 -0300

    feat: add react hook form to TaskDetails page

--- src/pages/task-details.jsx
+++ src/pages/task-details.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
+import { useForm } from "react-hook-form"
 import { Link, useNavigate, useParams } from "react-router-dom"
 import { toast } from "sonner"
 
@@ -15,17 +16,19 @@ import TimeSelect from "../components/TimeSelect"
 const TaskDetailsPage = () => {
   const { taskId } = useParams()
   const [task, setTask] = useState()
-  const [errors, setErrors] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
   const navigate = useNavigate()
-
-  const titleRef = useRef()
-  const timeRef = useRef()
-  const descriptionRef = useRef()
-
-  const handleBackClick = () => {
-    navigate(-1)
-  }
+  const {
+    register,
+    formState: { errors, isSubmitting },
+    handleSubmit,
+    reset,
+  } = useForm({
+    defaultValues: {
+      title: task?.title || "",
+      time: task?.time || "",
+      description: task?.description || "",
+    },
+  })
 
   useEffect(() => {
     const fetchTask = async () => {
@@ -36,6 +39,7 @@ const TaskDetailsPage = () => {
         }
         const data = await response.json()
         setTask(data)
+        reset(data)
       } catch (error) {
         console.error(error)
         toast.error("Erro ao buscar detalhes da tarefa")
@@ -43,10 +47,9 @@ const TaskDetailsPage = () => {
     }
 
     fetchTask()
-  }, [taskId])
+  }, [taskId, reset])
 
   const editTask = async (editedTask) => {
-    setIsLoading(true)
     try {
       const response = await fetch(`http://localhost:3000/tasks/${task.id}`, {
         method: "PATCH",
@@ -64,41 +67,13 @@ const TaskDetailsPage = () => {
       console.error(error)
       toast.error("Erro ao atualizar tarefa")
       return false
-    } finally {
-      setIsLoading(false)
     }
   }
 
-  const handleSaveClick = async () => {
-    const newErrors = []
-
-    const title = titleRef.current.value.trim()
-    const time = timeRef.current.value
-    const description = descriptionRef.current.value.trim()
-
-    if (!title) {
-      newErrors.push({
-        inputName: "title",
-        message: "O título é obrigatório.",
-      })
-    }
-    if (!time) {
-      newErrors.push({
-        inputName: "time",
-        message: "O horário é obrigatório.",
-      })
-    }
-    if (!description) {
-      newErrors.push({
-        inputName: "description",
-        message: "A descrição é obrigatória.",
-      })
-    }
-
-    if (newErrors.length > 0) {
-      setErrors(newErrors)
-      return
-    }
+  const handleSaveClick = async (data) => {
+    const title = data.title.trim()
+    const time = data.time
+    const description = data.description.trim()
 
     const editedTask = {
       title,
@@ -113,7 +88,6 @@ const TaskDetailsPage = () => {
   }
 
   const handleDeleteClick = async () => {
-    setIsLoading(true)
     try {
       const response = await fetch(`http://localhost:3000/tasks/${task.id}`, {
         method: "DELETE",
@@ -126,26 +100,20 @@ const TaskDetailsPage = () => {
     } catch (error) {
       console.error(error)
       toast.error("Erro ao deletar tarefa")
-    } finally {
-      setIsLoading(false)
     }
   }
 
-  const titleError = errors.find((error) => error.inputName === "title")
-  const timeError = errors.find((error) => error.inputName === "time")
-  const descriptionError = errors.find(
-    (error) => error.inputName === "description"
-  )
-
   return (
     <div className="w-full space-y-6 px-8 py-8">
+      {/* Back Button */}
       <button
         className="mb-4 flex h-7 w-7 items-center justify-center rounded-full bg-primary"
-        onClick={handleBackClick}
+        onClick={() => navigate(-1)}
       >
         <ArrowLeftIcon />
       </button>
 
+      {/* Breadcrumb */}
       <div className="mb-1 flex items-center gap-2 text-xs">
         <Link className="cursor-pointer text-text-gray" to="/">
           Minhas tarefas
@@ -154,6 +122,7 @@ const TaskDetailsPage = () => {
         <span className="font-semibold text-primary">{task?.title}</span>
       </div>
 
+      {/* Task Title */}
       <div className="flex w-full items-center justify-between">
         <h1 className="text-xl font-semibold">{task?.title}</h1>
 
@@ -163,52 +132,65 @@ const TaskDetailsPage = () => {
         </Button>
       </div>
 
-      <div className="space-y-6 rounded-xl bg-white p-6">
-        <div>
-          <Input
-            id="title"
-            label="Nome"
-            defaultValue={task?.title}
-            ref={titleRef}
-            disabled={isLoading}
-            errorMessage={titleError?.message}
-          />
+      {/* Edit Task Form */}
+      <form onSubmit={handleSubmit(handleSaveClick)}>
+        <div className="space-y-6 rounded-xl bg-white p-6">
+          <div>
+            <Input
+              id="title"
+              label="Nome"
+              {...register("title", {
+                required: "O título é obrigatório",
+                validate: (value) =>
+                  value.trim() !== "" || "O título não pode estar vazio",
+              })}
+              errorMessage={errors?.title?.message}
+              disabled={isSubmitting}
+            />
+          </div>
+
+          <div>
+            <TimeSelect
+              id="time"
+              label="Horário"
+              {...register("time", {
+                required: "O horário é obrigatório",
+                validate: (value) =>
+                  value.trim() !== "" || "O horário não pode estar vazio",
+              })}
+              errorMessage={errors?.time?.message}
+              disabled={isSubmitting}
+            />
+          </div>
+
+          <div>
+            <Input
+              id="description"
+              label="Descrição"
+              {...register("description", {
+                required: "A descrição é obrigatória",
+                validate: (value) =>
+                  value.trim() !== "" || "A descrição não pode estar vazio",
+              })}
+              errorMessage={errors?.description?.message}
+              disabled={isSubmitting}
+            />
+          </div>
         </div>
 
-        <div>
-          <TimeSelect
-            id="time"
-            label="Horário"
-            defaultValue={task?.time}
-            ref={timeRef}
-            disabled={isLoading}
-            errorMessage={timeError?.message}
-          />
+        {/* Save Button */}
+        <div className="flex w-full justify-end gap-3">
+          <Button
+            size="large"
+            color="primary"
+            type="submit"
+            disabled={isSubmitting}
+          >
+            {isSubmitting && <LoaderIcon className="h-4 w-4 animate-spin" />}
+            Salvar
+          </Button>
         </div>
-
-        <div>
-          <Input
-            id="description"
-            label="Descrição"
-            defaultValue={task?.description}
-            ref={descriptionRef}
-            disabled={isLoading}
-            errorMessage={descriptionError?.message}
-          />
-        </div>
-      </div>
-
-      <div className="flex w-full justify-end gap-3">
-        <Button
-          size="large"
-          color="primary"
-          onClick={handleSaveClick}
-          disabled={isLoading}
-        >
-          {isLoading && <LoaderIcon className="h-4 w-4 animate-spin" />}
-          Salvar
-        </Button>
-      </div>
+      </form>
     </div>
   )
 }
